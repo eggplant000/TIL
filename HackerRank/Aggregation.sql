d5fd879b87a7378a6cf72e89ef8b7dbb4f944a18
eggplant000 <ddorin09@gmail.com>
2021-11-05 18:55:14 +0900

    211105 sql practice

--- HackerRank/Aggregation.sql
+++ HackerRank/Aggregation.sql
@@ -6,3 +6,30 @@ SELECT COUNT(*) FROM city
 -- Revising Aggregations - Averages
 SELECT AVG(population) FROM city
     WHERE district = 'california';
+
+
+-- Average Population
+SELECT FLOOR(AVG(population)) FROM city;
+
+
+-- Japan Population
+SELECT SUM(population) FROM city
+    WHERE countrycode = 'JPN';
+
+
+-- Population Density Difference
+SELECT MAX(population)-MIN(population) FROM city;
+
+
+-- The Blunder
+SELECT CEIL(AVG(salary)-AVG(REPLACE(salary, 0, ''))) FROM employees;
+
+
+-- Top Earners
+SELECT months*salary, COUNT(*) FROM employee
+    WHERE months*salary = (SELECT MAX(months*salary) FROM employee);
+
+SELECT months*salary, COUNT(*) FROM employee
+    GROUP BY months*salary
+    ORDER BY months*salary DESC
+    LIMIT 1
