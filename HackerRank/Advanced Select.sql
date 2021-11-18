14d2c26914be4499f5917f78dcd995da3e6b186e
eggplant000 <ddorin09@gmail.com>
2021-11-18 23:45:49 +0900

    211118 sql practice

--- HackerRank/Advanced Select.sql
+++ HackerRank/Advanced Select.sql
@@ -9,3 +9,10 @@ GROUP BY occupation
 ORDER BY COUNT(*), occupation;
 
 
+-- Binary Tree Nodes
+SELECT DISTINCT *
+FROM (SELECT t1.N AS N, IF(ISNULL(t1.P), 'Root', IF(ISNULL(t2.P), 'Leaf', 'Inner')) 
+FROM BST t1
+LEFT JOIN BST t2 
+ON t1.N = t2.P) temp
+ORDER BY N;
